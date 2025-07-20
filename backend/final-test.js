6d3624c95f4a9432457406d7f1eae3667635d3c1
Ken <kenlee2002@outlook.com>
2025-07-20 20:07:11 +0900

    새 UI

--- backend/final-test.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-  mongoose.connect("mongodb+srv://ken:ken0224@minecraftcluster.eybdndl.mongodb.net/minecraft2d")
-  .then(() => console.log("연결 성공!"))
-  .catch(err => console.error("연결 실패:", err));
